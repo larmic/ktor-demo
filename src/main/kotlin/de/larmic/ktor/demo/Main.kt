07d1428736df3d153a9fb70cb95fde865afa7aaa
Lars Michaelis <l.michaelis.ext@ewe.de>
2021-01-12 16:44:10 +0100

    add ktor endpoint

--- src/main/kotlin/de/larmic/ktor/demo/Main.kt
+++ src/main/kotlin/de/larmic/ktor/demo/Main.kt
@@ -1,9 +1,23 @@
 package de.larmic.ktor.demo
 
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
 fun main() {
-    logger.info { "Hello Kotlin!" }
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                logger.info { "localhost:8080/ is called!" }
+                call.respondText("THIS IS KTOR!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
 }
